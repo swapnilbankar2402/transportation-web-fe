3a8aa2b501f07eb31a0093d50f21bb2883b5785b
Swapnil7 <swapnil@devbeeit.com>
2024-03-13 10:58:52 +0530

    added change for extra large screen

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -12,7 +12,7 @@ const LandingSection = () => {
       <Header />
       {/* <div className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-3" > */}
       {/* overflow-hidden py-10 sm:py-20 lg:pb-32 xl:pb-3 */}
-      <div className="py-4 md:grid md:grid-cols-5 md:gap-28 items-center">
+      <div className="py-4 2xl:py-32 md:grid md:grid-cols-5 md:gap-28 items-center">
         {/* Left section */}
         <div className="md:col-span-3  text-white">
           <h1 className="text-4xl font-semibold tracking-wide">
@@ -38,7 +38,10 @@ const LandingSection = () => {
         </div>
       </div>
       {/* Expore */}
-      <Link href={'/'} className="text-white cursor-pointer flex items-center gap-2">
+      <Link
+        href={"/"}
+        className="text-white cursor-pointer flex items-center gap-2"
+      >
         <div className=" bg-blurBg w-fit p-2 rounded-full">
           <IconifyIcons icon="ph:arrow-right-light" />
         </div>
